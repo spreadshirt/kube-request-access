f782ba96cdd20059884a626f45fdc063d632f5f7
Luna Stadler <luc@spreadshirt.net>
2022-10-24 17:29:58 +0200

    Parse an admission request
    This follows the example at [1], which is linked from the docs.
    
    [1]: https://github.com/kubernetes/kubernetes/blob/release-1.23/test/images/agnhost/webhook/main.go

---
+++ go.mod
@@ -0,0 +1,34 @@
+module git.spreadomat.net/deleng/kubectl-audit
+
+go 1.19
+
+require (
+	github.com/sirupsen/logrus v1.9.0
+	k8s.io/api v0.0.0-20221018235617-0ac6cf5e64f9
+	k8s.io/apimachinery v0.23.14-rc.0
+)
+
+require (
+	git.spreadomat.net/go/logging v0.2.1 // indirect
+	git.spreadomat.net/go/timing v0.0.0-20191108105106-fa93cad2583a // indirect
+	git.spreadomat.net/go/tracing v0.0.0-20190425125211-f1509a185eed // indirect
+	github.com/bshuster-repo/logrus-logstash-hook v0.0.0-20180418140028-1e961e8e173c // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/klog/v2 v2.30.0 // indirect
+	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
+	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
---
+++ kubectl-audit.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+
+	"git.spreadomat.net/go/logging"
+	"github.com/sirupsen/logrus"
+	admissionv1 "k8s.io/api/admission/v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+)
+
+var config struct {
+	Addr string
+}
+
+var scheme = runtime.NewScheme()
+var codecs = serializer.NewCodecFactory(scheme)
+
+func init() {
+	addToScheme(scheme)
+}
+
+func addToScheme(scheme *runtime.Scheme) {
+	utilruntime.Must(corev1.AddToScheme(scheme))
+	utilruntime.Must(admissionv1.AddToScheme(scheme))
+	utilruntime.Must(admissionregistrationv1.AddToScheme(scheme))
+}
+
+func main() {
+	flag.StringVar(&config.Addr, "addr", "localhost:1234", "Address to listen on")
+	flag.Parse()
+
+	http.HandleFunc("/", handleAdmission)
+
+	logrus.Infof("Listening on http://%s", config.Addr)
+	logrus.Fatal(http.ListenAndServe(config.Addr, nil))
+}
+
+func handleAdmission(w http.ResponseWriter, req *http.Request) {
+	logger := logging.LogForRequest(req)
+
+	var body []byte
+	if req.Body != nil {
+		if data, err := io.ReadAll(req.Body); err == nil {
+			body = data
+		}
+	}
+
+	// verify the content type is accurate
+	contentType := req.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		logger.Errorf("contentType=%s, expect application/json", contentType)
+		return
+	}
+	deserializer := codecs.UniversalDeserializer()
+	obj, gvk, err := deserializer.Decode(body, nil, nil)
+	if err != nil {
+		msg := fmt.Sprintf("Request could not be decoded: %v", err)
+		logger.Error(msg)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	var responseObj runtime.Object
+	switch *gvk {
+	case admissionv1.SchemeGroupVersion.WithKind("AdmissionReview"):
+		requestedAdmissionReview, ok := obj.(*admissionv1.AdmissionReview)
+		if !ok {
+			logger.Errorf("Expected v1.AdmissionReview but got: %T", obj)
+			return
+		}
+		responseAdmissionReview := &admissionv1.AdmissionReview{}
+		responseAdmissionReview.SetGroupVersionKind(*gvk)
+		responseAdmissionReview.Response = handle(requestedAdmissionReview)
+		responseAdmissionReview.Response.UID = requestedAdmissionReview.Request.UID
+		responseObj = responseAdmissionReview
+	default:
+		msg := fmt.Sprintf("Unsupported group version kind: %v", gvk)
+		logger.Error(msg)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	// klog.V(2).Info(fmt.Sprintf("sending response: %v", responseObj))
+	respBytes, err := json.Marshal(responseObj)
+	if err != nil {
+		logger.Error(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := w.Write(respBytes); err != nil {
+		logger.Error(err)
+	}
+}
+
+func handle(admissionReview *admissionv1.AdmissionReview) *admissionv1.AdmissionResponse {
+	panic("not implemented")
+}
